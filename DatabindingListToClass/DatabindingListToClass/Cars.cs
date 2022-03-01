2d11d7f019027c960dd49a7805b843293eede16e
Marek Piotrowski <marekp18@gmail.com>
2022-03-01 13:49:33 +0100

    [43] Databinding List to Class

---
+++ DatabindingListToClass/DatabindingListToClass/Cars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabindingListToClass
+{
+    class Cars
+    {
+        public static List<Car> GetCars()
+        {
+            return new List<Car>()
+            { 
+                new Car(){Owner="Mike", Type=CarType.Hatchbak, Brand=CarBrand.Audi},
+                new Car(){Owner="Emma", Type=CarType.Sedan, Brand=CarBrand.Tesla},
+                new Car(){Owner="Jon", Type=CarType.SUV, Brand=CarBrand.BMW}
+            }.ToList();
+        }
+    }
+}
