febc1ec36b76d50c3fbce7f082d648342bece419
Marek Piotrowski <marekp18@gmail.com>
2022-03-01 13:30:06 +0100

    [42] Databinding with enum ObjectDataProvider Combobox

---
+++ DataBindingComboBoxEnumDemo/DataBindingComboBoxEnumDemo/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingComboBoxEnumDemo
+{
+    public enum Animal
+    {
+        Dog,
+        Cat,
+        Mouse,
+        Elephant,
+        Giraffe
+    }
+}
---
+++ DataBindingComboBoxEnumDemo/DataBindingComboBoxEnumDemo/Taste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingComboBoxEnumDemo
+{
+    /* 
+     * xmlns:sys="clr-namespace:System;assembly=System;assembly=mscorlib"
+     * need to be added to MainWindow.xaml to recognize enum 
+     */
+    public enum Taste
+    {
+        Sweet,
+        Sour,
+        Bitter,
+        Salty
+    }
+}
