d98d97a8e807d7f5cbaa0a72cf41f46ba77d9551
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-03-08 10:34:34 +0100

    [72] Creating our own Dependency Property and using it (propdp + Tab + Tab)

--- DependencyPropertiesColorChanging/DependencyPropertiesColorChanging/MainWindow.xaml.cs
+++ DependencyPropertiesColorChanging/DependencyPropertiesColorChanging/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DependencyPropertiesColorChanging
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyUCinMain.Awesomeness += 1000;
+        }
     }
 }
