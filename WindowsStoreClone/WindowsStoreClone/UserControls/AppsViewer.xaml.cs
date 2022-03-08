f603da25193909ec79af4769e1103f1a8181e921
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-03-08 11:55:08 +0100

    [76] Displaying multiple Items in a ScrollViewer part 1 - scrolling by clicking

---
+++ WindowsStoreClone/WindowsStoreClone/UserControls/AppsViewer.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindowsStoreClone.UserControls
+{
+    /// <summary>
+    /// Interaction logic for AppsViewer.xaml
+    /// </summary>
+    public partial class AppsViewer : UserControl
+    {
+        List<AnApp> PresentedApps;
+
+        public AppsViewer()
+        {
+            InitializeComponent();
+            PresentedApps = new List<AnApp>();
+            AppsList.ItemsSource = PresentedApps;
+            for(int i = 0; i < 9; i++)
+            {
+                AnApp curr = new AnApp();
+                PresentedApps.Add(curr);
+            }
+
+        }
+
+        private void ScrollLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ScrollRightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
