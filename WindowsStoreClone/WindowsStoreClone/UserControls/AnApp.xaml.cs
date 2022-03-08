9ebfcf8eeaa87d4be0a800176ef75b8fff657e47
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-03-08 11:39:39 +0100

    [75] Setup and Display an app part 2

--- WindowsStoreClone/WindowsStoreClone/UserControls/AnApp.xaml.cs
+++ WindowsStoreClone/WindowsStoreClone/UserControls/AnApp.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MiscUtil;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,25 @@ namespace WindowsStoreClone.UserControls
     /// </summary>
     public partial class AnApp : UserControl
     {
+        public string AppName;
+        public ImageSource AppImageSource;
+
         public AnApp()
         {
             InitializeComponent();
+            List<string> filepaths = Directory.GetFiles(Environment.CurrentDirectory
+                + @"\..\..\Images", "*.png").ToList<string>();
+            FileInfo myRandomFile = new FileInfo(filepaths[StaticRandom.Next(filepaths.Count)]);
+            ProductImage.Source = new BitmapImage(new Uri(myRandomFile.FullName, UriKind.RelativeOrAbsolute));
+            AppNameText.Text = (new CultureInfo("en-US", false).TextInfo).ToTitleCase(
+                myRandomFile.FullName // C:\0git\WPF_Guide_for_beginners\WindowsStoreClone\WindowsStoreClone\Images\052-snapchat.png
+                .Split('\\').Last()   // 052-snapchat.png
+                .Split('-').Last()    // snapchat.png
+                .Split('.').First()   // snapchat
+                );
+
+            AppName = AppNameText.Text.ToString();
+            AppImageSource = ProductImage.Source;
         }
 
         private void ProductImage_MouseUp(object sender, MouseButtonEventArgs e)
