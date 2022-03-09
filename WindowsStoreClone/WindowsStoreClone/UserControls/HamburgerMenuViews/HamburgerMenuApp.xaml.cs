a31aa22bb36346789765ae75e0f5accbcbdaebcb
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-03-09 17:43:28 +0100

    [100] Creating the HamburgerMenuHeader

--- WindowsStoreClone/WindowsStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuApp.xaml.cs
+++ WindowsStoreClone/WindowsStoreClone/UserControls/HamburgerMenuViews/HamburgerMenuApp.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MiscUtil;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,36 @@ namespace WindowsStoreClone.UserControls.HamburgerMenuViews
     /// </summary>
     public partial class HamburgerMenuApp : UserControl
     {
+        public List<string> AppNames;
+        public List<string> AppTypes;
+        public string AppName;
+        public DateTime Purchased;
+        public string Type;
+
         public HamburgerMenuApp()
         {
             InitializeComponent();
+
+            AppTypes = new List<string>()
+            {
+                "Apps",
+                "Games",
+                "Movies",
+                "Avatars"
+            };
+
+            List<string> filepaths = Directory.GetFiles(Environment.CurrentDirectory + @"\..\..\Images\MiniIcons", "*.png").ToList<string>();
+            FileInfo myRandomFile = new FileInfo(filepaths[StaticRandom.Next(filepaths.Count)]);
+            AppImage.Source = new BitmapImage(new Uri(myRandomFile.FullName, UriKind.RelativeOrAbsolute));
+            AppNameLabel.Content = (new CultureInfo("en-US", false).TextInfo).ToTitleCase
+                (
+                AppImage.Source.ToString().Split('/').Last().Split('-').Last().Split('.').First()
+                );
+            AppName = AppNameLabel.Content.ToString();
+            Type = AppTypes[StaticRandom.Next(AppTypes.Count)];
+            Purchased = new DateTime(2021, 1, StaticRandom.Next(1, DateTime.Now.Day + 1));
+            PuraschedLabel.Content = "Purasched" + Purchased.ToString("d");
+
         }
     }
 }
