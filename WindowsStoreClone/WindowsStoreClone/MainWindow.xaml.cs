76b8ac18f43a9aab6095146d161ef9d336efd4b9
Marek Piotrowski <marekpiotrowski2@icpt.pl>
2022-03-10 09:23:05 +0100

    [102] Implementing MahApps

--- WindowsStoreClone/WindowsStoreClone/MainWindow.xaml.cs
+++ WindowsStoreClone/WindowsStoreClone/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +21,7 @@ namespace WindowsStoreClone
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
 
         private Main MainWindowContentPage;
